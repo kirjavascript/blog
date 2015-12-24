fa5b07c7d70b8e3ed432d8a0c3e18514e084a7cf
snkenjoi <snkenjoi@gmail.com>
2015-12-24 13:26:10 +0000

    main viewport

--- src/js/viewer.js
+++ src/js/viewer.js
@@ -1,32 +1,51 @@
 function posts() {
 
+    //viewer.selectAll("*").transition().duration(sp).style("opacity", 0).remove();
+
     // setup container for mouse events
     viewer
         .attr({
             x:x(120),y:y(120),
             width:c.w-x(120),
             height: c.h-y(240),
-            viewBox:[x(120),y(120),c.w-x(120),c.h-y(240)].join(" ")
-        }).style("cursor","move")
-        .call(zoom);
+            //viewBox: [x(120), y(120), c.w - x(120), c.h - y(240)].join(" ")
+        })
+        //.style("cursor","move")
+        //.call(zoom);
 
     // setup viewport
     vp = viewer.append("g")
 
-    vp.append("rect")
-        .attr({
-            fill:"#DDD",
-            x:x(120),y:y(120),
-            width:240,
-            height:240
-        })
+    d3.json("json/posts.json", (e,data) => {
+        var posts = vp.selectAll(".post")
+            .data(data)
+
+            // bbox snap
+            // use native js scroll
+
+        var postGroup = posts.enter()
+            .append("g")
+            .attr("class","post")
+
+        postGroup.append("foreignObject")
+            .attr("x", x(25))
+            .attr("y", (d,i) => x(25) + i* y(475))
+            .attr("width",x(700))
+            .attr("height",y(400))
+            .append("xhtml:body")
+            .style("background-color","#DDD")
+            .html(d => d.title);
+
+    })
 
 }
 
 function zoomed() {
-    vp.attr("transform", "translate(" + d3.event.translate + ")scale(" + d3.event.scale + ")");
+    // console.log(d3.event.scale)
+    // vp.attr("transform", "translate(0," + d3.event.translate[1] + ")scale(" + d3.event.scale + ")");
+    vp.attr("transform", "translate(0," + d3.event.scale + ")");
 }
 
-var zoom = d3.behavior.zoom()
-    .scaleExtent([0.1, 20])
-    .on("zoom", zoomed);
+// var zoom = d3.behavior.zoom()
+//     .scaleExtent([2000, 1])
+//     .on("zoom", zoomed);
