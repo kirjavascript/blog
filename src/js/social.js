b8329e50487fd7445cc9156fe17205a1b3e56c6f
snkenjoi <snkenjoi@gmail.com>
2015-12-28 14:14:38 +0000

    social APIs implemented

--- src/js/social.js
+++ src/js/social.js
@@ -4,22 +4,26 @@ function social() {
         {
             name:"youtube",
             colour:'#b31217',
-            path: "M400 224c144 0 201 2 224 25 17 17 26 52.125 26 151s-9 134-26 151c-23 23-80 25-224 25s-201-2-224-25c-17-17-26-52.125-26-151s9-134 26-151c23-23 80-25 224-25zm-52 100v141l135-70z"
+            path: "M400 224c144 0 201 2 224 25 17 17 26 52.125 26 151s-9 134-26 151c-23 23-80 25-224 25s-201-2-224-25c-17-17-26-52.125-26-151s9-134 26-151c23-23 80-25 224-25zm-52 100v141l135-70z",
+            click:youtube
         },
         {
             name:"blocks",
             colour:'#FA0',
-            path:"M140 482V320c0-9 5-15 10-18l238-158c8-5 16-5 24 0l238 158c6.24 3.743 10 12 10 19v158c0 9-5 15-10 19L412 656c-8 5-17 5-24 0L150 497c-9-6-10-11-10-15zm282-278v104l97 65 78-52zm-44 104V204L203 321l78 52zm-193 54v75l56-37zm193 234V492l-97-65-78 52zm22-143l79-53-79-53-79 53zm22 143l175-117-78-52-97 64v105zm193-159v-75l-56 38z"
+            path:"M140 482V320c0-9 5-15 10-18l238-158c8-5 16-5 24 0l238 158c6.24 3.743 10 12 10 19v158c0 9-5 15-10 19L412 656c-8 5-17 5-24 0L150 497c-9-6-10-11-10-15zm282-278v104l97 65 78-52zm-44 104V204L203 321l78 52zm-193 54v75l56-37zm193 234V492l-97-65-78 52zm22-143l79-53-79-53-79 53zm22 143l175-117-78-52-97 64v105zm193-159v-75l-56 38z",
+            click:blocks
         },
         {
             name:"twitter",
             colour:'#00aced',
-            path:"M679 239s-21 34-55 57c7 156-107 329-314 329-103 0-169-50-169-50s81 17 163-45c-83-5-103-77-103-77s23 6 50-2c-93-23-89-110-89-110s23 14 50 14c-84-65-34-148-34-148s76 107 228 116c-22-121 117-177 188-101 37-6 71-27 71-27s-12 41-49 61c30-2 63-17 63-17z"
+            path:"M679 239s-21 34-55 57c7 156-107 329-314 329-103 0-169-50-169-50s81 17 163-45c-83-5-103-77-103-77s23 6 50-2c-93-23-89-110-89-110s23 14 50 14c-84-65-34-148-34-148s76 107 228 116c-22-121 117-177 188-101 37-6 71-27 71-27s-12 41-49 61c30-2 63-17 63-17z",
+            click:twitter
         },
         {
             name:"github",
             colour:'purple',
-            path:"M400 139c144 0 260 116 260 260 0 115-75 213-178 247-9 3-17-2-17-13v-71c0-35-18-48-18-48 57-6 119-28 119-128 0-44-27-70-27-70s14-29-2-69c0 0-22-7-72 27-42-12-88-12-130 0-50-34-72-27-72-27-16 40-2 69-2 69s-27 26-27 70c0 100 62 122 119 128 0 0-14 10-17 35-15 7-53 18-76-22 0 0-13-25-39-27 0 0-26 0-2 16 0 0 17 8 29 38 0 0 16 51 88 35v44c0 11-9 16-18 13-103-34-178-132-178-247 0-144 116-260 260-260z"
+            path:"M400 139c144 0 260 116 260 260 0 115-75 213-178 247-9 3-17-2-17-13v-71c0-35-18-48-18-48 57-6 119-28 119-128 0-44-27-70-27-70s14-29-2-69c0 0-22-7-72 27-42-12-88-12-130 0-50-34-72-27-72-27-16 40-2 69-2 69s-27 26-27 70c0 100 62 122 119 128 0 0-14 10-17 35-15 7-53 18-76-22 0 0-13-25-39-27 0 0-26 0-2 16 0 0 17 8 29 38 0 0 16 51 88 35v44c0 11-9 16-18 13-103-34-178-132-178-247 0-144 116-260 260-260z",
+            click:github
         },
     ];
 
@@ -28,18 +32,15 @@ function social() {
 
     var goo = s.append("g");
 
+    window.rGoo = d => {
+        goo.attr({
+            transform: (d,i) =>
+            'translate('+[x(1180*2)-((i+1)*157),y(800*2)-157]+')',
+        })
+    };
 
+    rGoo();
 
-    setInterval(() => {
-        //line.transition().duration(sp)
-    },sp)
-
-
-
-    goo.attr({
-        transform: (d,i) =>
-        'translate('+[x(1180*2)-((i+1)*157),y(800*2)-157]+')'
-    })
     var filter = gooey(goo);
 
     goo.on("mousemove", function(){circleFollow(this,items)})
@@ -63,6 +64,7 @@ function social() {
             .attr("cy", 50)
             .attr("r", 50)
             .style("fill", d => d.colour)
+            .on('click' , d => {typeof d.click=="function"&&d.click()})
 
     var social = goo.append("g")
         .attr("transform","scale(0.12)")
@@ -75,8 +77,8 @@ function social() {
         .attr("class", "socialIcon")
         .style("fill", "#FFF")
         .attr("transform",(d,i) => "translate("+[-(i)*1310,20]+")")
+        .on('click' , d => {typeof d.click=="function"&&d.click()})
 };
-
 
 function circleFollow(self,items) {
         var circle = d3.selectAll(".gooCircle");
@@ -104,7 +106,7 @@ function gooey(container) {
 	var filter =  defs.append('filter').attr('id','gooey');
 	filter.append('feGaussianBlur')
 		.attr('in','SourceGraphic')
-		.attr('stdDeviation','25')
+		.attr('stdDeviation','15')
 		.attr('result','blur');
 	filter.append('feColorMatrix')
 		.attr('in','blur')
@@ -115,4 +117,145 @@ function gooey(container) {
 		.attr('in','SourceGraphic')
 		.attr('in2','gooey');
         return filter;
+}
+
+// APIs //
+
+function github() {
+    d3.json('https://api.github.com/users/snkenjoi/repos', (e,data) => {
+        var gitShapes = [];
+        data.forEach((o,i) => {
+            gitShapes.push({
+                    "shape": "foreignObject",
+                    "size": [
+                        x(600),
+                        y(100)
+                    ],
+                    "scale":0.7,
+                    "style": {
+                        "background-color": "purple",
+                    },
+                    "html": [
+                        "<a target=\"_blank\" href="+o.html_url+">",
+                            "<div class=\"socialBox\">",
+                                "<span>"+o.name+"</span>",
+                                "<div>"+o.language+"</div>",
+                                "<p>"+o.description+"</p>",
+                            "</div>",
+                        "</a>"
+                    ],
+                    "foci": {
+                        "x": i%2==0?-200:1500,
+                        "y": 175*(i-1.5)
+                    }
+                })
+        })
+        d3on(gitShapes);
+    })
+}
+
+function blocks() {
+    d3.json('https://api.github.com/users/snkenjoi/gists', (e,data) => {
+        console.log(data)
+        var blox = [];
+        data.forEach((o,i) => {
+            blox.push({
+                    "shape": "foreignObject",
+                    "size": [
+                        x(600),
+                        y(100)
+                    ],
+                    "scale":0.7,
+                    "html": [
+                        "<a target=\"_blank\" href=\"http://bl.ocks.org/"+o.id+"\">",
+                            "<div class=\"socialBox blox\">",
+                                "<p>"+o.description+"</p>",
+                            "</div>",
+                        "</a>"
+                    ],
+                    "foci": {
+                        "x": i%2==0?-200:1500,
+                        "y": 175*(i-1.5)
+                    }
+                })
+        })
+        d3on(blox);
+    })
+}
+
+function twitter() {
+    var m = document.createElement("script");
+        m.type = "text/javascript";
+        m.src =
+            "https://cdn.syndication.twimg.com/widgets/timelines/654442445963440128?&lang=en"+ "&callback=twitterCallback&suppress_response_codes=true&rnd=" + Math.random();
+    document.getElementsByTagName("body")[0].appendChild(m);
+}
+
+function twitterCallback(data) {
+    var tShapes = [];
+    var c = document.createElement("div");
+                c.innerHTML = data.body;
+
+    var msgs = c.getElementsByClassName("e-entry-title");
+    var urls = c.getElementsByClassName("permalink");
+    var nick = c.getElementsByClassName("p-nickname")[0].innerHTML;
+    var dates = c.getElementsByClassName("dt-updated");
+    var dump = [];
+
+    for(var i=0;i<msgs.length;i++) {
+        dump.push({
+            tweet: msgs[i].innerHTML,
+            url: urls[i].getAttribute("href"),
+            date: dates[i].getAttribute("aria-label")
+        })
+    }
+
+    dump.forEach((o,i) => {
+        tShapes.push({
+                "shape": "foreignObject",
+                "size": [
+                    x(600),
+                    y(200)
+                ],
+                "scale":0.7,
+                "html": [
+                    "<a target=\"_blank\" href="+o.url+">",
+                        "<div class=\"socialBox tShapes\">",
+                            ""+o.tweet+"",
+                            "<div>"+o.date+"</div>",
+                        "</div>",
+                    "</a>"
+                ],
+                "foci": {
+                    "x": i%2==0?-200:1500,
+                    "y": 125*(i-2)
+                }
+            })
+    })
+    d3on(tShapes);
+}
+
+function youtube() {
+    d3.json("https://www.googleapis.com/youtube/v3/search?key=AIzaSyBadqgPm8xQrJHJGh7LL5BOzoV1LYwc6cY&channelId=UCnmMB5r3-dXMx0cX3vdb2aQ&part=snippet,id&order=date&maxResults=11", (e,data) => {
+        var yShapes = [];
+        data.items.forEach((o,i) => {
+            yShapes.push({
+                    "shape": "foreignObject",
+                    "size": [
+                        300,
+                        230
+                    ],
+                    "html": [
+                        "<a target=\"_blank\" href=https://www.youtube.com/watch?v="+o['id']['videoId'] + ">",
+                                "<img src=\"http://img.youtube.com/vi/"+o['id']['videoId']+"/0.jpg\" style=\"width:300px;height:230px\">",
+                        "</a>"
+                    ],
+                    "foci": {
+                        "x": -500+(600*(i/2)),
+                        "y": -200+(i%2==0?460:0)
+                    }
+                })
+        })
+        d3on(yShapes);
+    })
 }
--- src/js/viewer.js
+++ src/js/viewer.js
@@ -1,26 +1,56 @@
 function viewport() {
     // setup container
     viewer
-        .attr({
-            x:0,y:y(120),
-            width:c.w,
-            height: c.h,
-            viewBox: [0, y(120), c.w, c.h - y(240)].join(" ")
-        })
+        .attr(viewAttr())
         //.style("cursor","move")
         //.call(zoom);
 
     // setup viewport
     vp = viewer.append("g")
 
-
-    d3on("posts/0.json")
+    getLatestPost();
 }
 
-function post(d) {
-    // add nav, tags, comments, etc
-    // comments -> allow not clearing
-    // d3 code in json?
-    //<iframe src=\"http://bl.ocks.org/snkenjoi/raw/a8555a26b14f4088bf51/\"></iframe>
-    return d;
+function viewAttr() {
+    return {
+        x:0,y:y(120),
+        width:c.w,
+        height: c.h,
+        viewBox: [0, y(120), c.w, c.h - y(240)].join(" ")
+    };
+}
+
+function getLatestPost() {
+    d3.json("json/posts.json", (e,d) => {
+        //grab latest post
+        post(d[0]);
+    })
+}
+
+function post(data) {
+    d3on(data.json,null, d => {
+
+        document.title = data.title;
+
+        // add tags
+        d.push({
+            "shape": "foreignObject",
+            "size": [600,100],
+            "html": [
+                "<p class=\"tags\">tags: "+data.tags+"</p>",
+                "share: ",
+                "<a href=\"?"+encodeURIComponent(data.title).replace(/\ /g,'+')+"\">permalink</a>"
+            ],
+            "foci": {
+                "x": -300,
+                "y": 900
+            }
+        })
+
+        // add nav, tags, comments, etc
+        // comments -> allow not clearing
+        // d3 code in json?
+        //<iframe src=\"http://bl.ocks.org/snkenjoi/raw/a8555a26b14f4088bf51/\"></iframe>
+        return d;
+    })
 }
